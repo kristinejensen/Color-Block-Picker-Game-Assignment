069dce83d135cf767ab2f3f5f6f62c5e728c188c
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-14 21:05:42 -0600

    Added and tested random number generator function

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ // waits for document to loaded
+$(document).ready(function(){ // waits for document to load
   $('#pickAColorPrompt').text("Hey, pick a random color!"); // prompts the user to pick a color
   $('#colorBlocks').append( // displays color blocks on DOM
     '<div>' + $('#redBlock') + '</div>' +
@@ -22,14 +22,17 @@ $(document).ready(function(){ // waits for document to loaded
     // logs out the color block value at array index 0 - used for testing
     // console.log(colorBlockArray[0].value);
 
-  });
+  $('.button').on('click', function(){ // event listener for color block click
+    randomNumber(1, 4);  //this will be used to call the function when the color block is clicked
 
-//     $('.button').on('click', function(){
-//
-//       randomNumber(1, 4);
-//
-//
-//
-//       function randomNumber(min, max){
-//         return Math.floor(Math.random() * (1 + max - min) + min);
+    function randomNumber(min, max){
+      return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+  });
+});
+
+// colorBlockArry[i].value; --> not sure if I'll need this, but I think I do.
+
+// for (var i = 0; i < colorBlockArray.length; i++) {
+//   array[i]
 // }
