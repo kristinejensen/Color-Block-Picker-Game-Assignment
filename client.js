e27235caac8951cfda5deb10f8b40d3affab0f1a
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-14 20:20:29 -0600

    added data values to divs

--- client.js
+++ client.js
@@ -5,4 +5,21 @@ $(document).ready(function(){ // waits for document to loaded
     '<div>'+ $('#greenBlock') + '</div>' +
     '<div>' + $('#yellowBlock') + '</div>' +
     '<div>' + $('#blueBlock') + '</div>');
+
+    var redBlock = $('#redBlock')
+    var greenBlock = $('#greenBlock')
+    var yellowBlock = $('#yellowBlock')
+    var blueBlock = $('#blueBlock')
+
+    redBlock.data('value', 1);
+    greenBlock.data('value', 2);
+    yellowBlock.data('value', 3);
+    blueBlock.data('value', 4);
+
+    $('.button').on('click', function(){
+      console.log('i clicked a button');
+
+
+    });
+
   });
